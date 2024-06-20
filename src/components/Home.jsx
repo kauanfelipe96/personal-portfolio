87c3cd35102d1ca98ea70dc2b439f5fc6c9f9daf
Kauan Felipe <kauan_falima@hotmail.com>
2024-06-19 23:11:33 -0300

    updating home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
 import { ReactTyped } from "react-typed";
+import brasil from '../assets/images/brazil-icon.png'
+import usa from '../assets/images/usa-icon.png'
 
 export default function Home() {
   return (
@@ -26,6 +28,10 @@ export default function Home() {
           <a href="https://www.linkedin.com/in/kauanfelipe96/" target="_blank"><FaLinkedin className="text-[25px] hover:text-blue-800" alt="Link para perfil do LinkedIn" /></a>
         </div>
         <button className="bg-blue-500 text-white font-bold rounded-full px-8 py-2 text-center mt-5 hover:scale-110 duration-500 hover:bg-blue-700">Curriculum Vitae</button>
+        <div className="flex gap-5 mt-5">
+          <img src={brasil} alt="bandeira do Brasil" className="w-[50px] hover:scale-125 duration-500 rounded-md" />
+          <img src={usa} alt="bandeira" className="w-[50px] hover:scale-125 duration-500 rounded-md" />
+        </div>
         <a href="#about" className="absolute bottom-5">
           <div className="flex flex-col items-center">
             <p className="mb-3 text-blue-500 font-light">Ver mais</p>
